7db064277fa97da08e550a7ff56c2d83c6680439
Dragos Ginjoveanu <dragosginjoveanu@Dragoss-MacBook-Pro.local>
2025-10-30 14:07:36 +0200

    finish lab4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Ginjoveanu_Dragos_Lab2.Data;
 using Ginjoveanu_Dragos_Lab2.Models;
+using Ginjoveanu_Dragos_Lab2.Models.ViewModels;
 
 namespace Ginjoveanu_Dragos_Lab2.Pages.Categories
 {
@@ -20,12 +21,29 @@ namespace Ginjoveanu_Dragos_Lab2.Pages.Categories
         }
 
         public IList<Category> Category { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        
+        public int BookID { get; set; }
+        public int CategoryID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+                .Include(c => c.BookCategories)       // Include(Category -> BookCategory)
+                .ThenInclude(bc => bc.Book)       // ThenInclude(BookCategory -> Book)
+                .ThenInclude(b => b.Author)   // ThenInclude(Book -> Author)
+                .OrderBy(c => c.CategoryName)
+                .AsNoTracking()
+                .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+    
+                Category category = CategoryData.Categories
+                    .Where(i => i.ID == id.Value).Single();
+    
+                // Corecția este aici:
+                CategoryData.Books = category.BookCategories.Select(bc => bc.Book);
             }
         }
     }
